1d76c21c0b051f8321e5887fa8271be55d7cb16d
Jesse Brinkley <jbrink28@gmail.com>
2020-08-20 03:31:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,18 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+speak_to_grandma = "I LOVE YOU GRANDMA!"
+if grandma == "does hear"
+  puts "I LOVE YOU TOO PUMKPIN!"
+elsif grandma == "barely hears"
+  puts "NO, NOT SINCE 1938!"
+else grandma == "can't hear"
+  puts "HUH?! SPEAK UP, SONNY!"
+end
