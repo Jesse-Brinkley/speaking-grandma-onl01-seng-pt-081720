c8cf1337f5fad74036adc6de49cd690372ef234f
Jesse Brinkley <jbrink28@gmail.com>
2020-08-20 03:47:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,11 +11,11 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
-grandma = "I LOVE YOU GRANDMA!"
-if grandma == "does hear"
+speak_to_grandma = "I LOVE YOU GRANDMA!"
+if speak_to_grandma == "does hear"
   puts "I LOVE YOU TOO PUMKPIN!"
-elsif grandma == "barely hears"
+elsif speak_to_grandma == "barely hears"
   puts "NO, NOT SINCE 1938!"
-else grandma == "can't hear"
+else speak_to_grandma == "can't hear"
   puts "HUH?! SPEAK UP, SONNY!"
 end
