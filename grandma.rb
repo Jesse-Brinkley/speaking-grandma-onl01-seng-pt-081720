ad62e75b6ac4d6e846059cf123542ce903097650
Jesse Brinkley <jbrink28@gmail.com>
2020-08-20 03:57:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,6 +16,10 @@ if speak_to_grandma == "I LOVE YOU GRANDMA"
   puts "I LOVE YOU TOO PUMKPIN!"
 elsif speak_to_grandma == "Hi, Nana, how are you?"
   puts "HUH?! SPEAK UP, SONNY!"
-else speak_to_grandma == "Hi!"
+elsif speak_to_grandma == "Hi!"
   puts "HUH?! SPEAK UP, SONNY!"
+elsif speak_to_grandma == 'WHAT DID YOU EAT TODAY?'
+  puts "NO, NOT SINCE 1938!"
+else speak_to_grandma == 'WHAT?'
+  puts "NO, NOT SINCE 1938!" 
 end
