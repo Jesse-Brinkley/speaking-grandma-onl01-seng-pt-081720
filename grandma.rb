5931dd8373b435cf8c5aafdf9cca58c41dc287e1
Jesse Brinkley <jbrink28@gmail.com>
2020-08-20 04:54:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,15 +13,15 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma
   puts "speak_to_grandma"
-if speak_to_grandma == phrase (I LOVE YOU GRANDMA!)
+if speak_to_grandma == phrase(I LOVE YOU GRANDMA!)
   puts "I LOVE YOU TOO PUMKPIN!"
-elsif speak_to_grandma == phrase (Hi, Nana, how are you?)
+elsif speak_to_grandma == phrase(Hi, Nana, how are you?)
   puts "HUH?! SPEAK UP, SONNY!"
-elsif speak_to_grandma == phrase (Hi!)
+elsif speak_to_grandma == phrase(Hi!)
   puts "HUH?! SPEAK UP, SONNY!"
-elsif speak_to_grandma == phrase (WHAT DID YOU EAT TODAY?)
+elsif speak_to_grandma == phrase(WHAT DID YOU EAT TODAY?)
   puts "NO, NOT SINCE 1938!"
-else speak_to_grandma == phrase (WHAT?)
+else speak_to_grandma == phrase(WHAT?)
   puts "NO, NOT SINCE 1938!"
 end
 end
