a2c03963a0e7e5c99c0d6fab650265108bf9e48b
Jesse Brinkley <jbrink28@gmail.com>
2020-08-20 15:14:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMKPIN!"
+  "I LOVE YOU TOO PUMKPIN!"
 elsif speak_to_grandma == phrase('Hi Nana, how are you?')
   puts "HUH?! SPEAK UP, SONNY!"
 elsif speak_to_grandma == phrase('Hi!')
